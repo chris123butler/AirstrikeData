a39c9c4b9557054db06072fe1a788e0ff9639ac5
Kyle Dahlgren <kdahlgr1@uncc.edu>
2020-02-25 16:40:36 -0500

    commented out PyQt stuff, currently have two buttons working using Tkinter and the browse button outputs the chosen directory to command line, going to have to change obviously to allow selection of pdf

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
 from PyQt5.QtWidgets import *
+import sys
+from tkinter import *
+from tkinter import filedialog
+# app = QApplication([])
+# window = QWidget()
+# layout = QVBoxLayout()
+# layout.addWidget(QPushButton('Go'))
+# layout.addWidget(QPushButton('Bye'))
+# window.setLayout(layout)
+# window.resize(800,400)
+# window.show()
+#
+# app.exec_()
+def browse_button():
+    # Allow user to select a directory and store it in global var
+    # called folder_path
+    global folder_path
+    filename = filedialog.askdirectory()
+    folder_path.set(filename)
+    print(filename)
 
-app = QApplication([])
-window = QWidget()
-layout = QVBoxLayout()
-layout.addWidget(QPushButton('Go'))
-layout.addWidget(QPushButton('Bye'))
-window.setLayout(layout)
-window.show()
-app.exec_()
+root = Tk()
+
+button1 = Button(text="Previous Data")
+folder_path = StringVar()
+lbl1 = Label(master=root,textvariable=folder_path)
+lbl1.grid(row=0, column=1)
+button2 = Button(text="Browse", command=browse_button)
+button1.grid(row=0, column=1)
+button2.grid(row=1, column=1)
+mainloop()
