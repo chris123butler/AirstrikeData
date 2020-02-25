34d55b0148909e15ed298c656ca687dc032fa066
Kyle Dahlgren <kdahlgr1@uncc.edu>
2020-02-25 16:50:18 -0500

    changed so both can handle to choose a directory, however receiving error when upon choosing directory it should fill into the label

--- app.py
+++ app.py
@@ -21,12 +21,14 @@ def browse_button():
     print(filename)
 
 root = Tk()
-
-button1 = Button(text="Previous Data")
+#geometry handles height&width of frame
+root.geometry("600x400")
+button1 = Button(text="Previous Data", command=browse_button)
 folder_path = StringVar()
 lbl1 = Label(master=root,textvariable=folder_path)
 lbl1.grid(row=0, column=1)
 button2 = Button(text="Browse", command=browse_button)
 button1.grid(row=1, column=1)
-button2.grid(row=10, column=20)
+button2.grid(row=10, column=1)
+
 mainloop()
