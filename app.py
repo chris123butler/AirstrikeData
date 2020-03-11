cc603969a1247c9b0f612bd692366bf665a5be3f
Chris Butler <cbutle51@uncc.edu>
2020-03-11 13:54:28 -0400

    Working product with output file
    IT LIVES

--- app.py
+++ app.py
@@ -1,5 +1,19 @@
 # import the library
 from appJar import gui
+import pdf_extractor as ex
+
+exist = "Optional"
+out = "Select path..."
+
+# TODO REMOVE THIS
+urls = ['https://www.inherentresolve.mil/Portals/14/Documents/Strike%20Releases/2015/04April/1%20Apr%20Strike%20Release.pdf?ver=2017-01-13-131120-810',
+       'https://www.inherentresolve.mil/Portals/14/Documents/Strike%20Releases/2015/07July/20150707%20Strike%20Release%20final.pdf?ver=2017-01-13-131141-437',
+       'https://www.inherentresolve.mil/Portals/14/Documents/Strike%20Releases/2017/01January/20170125%20Strike%20Release%20Final.pdf?ver=2017-01-25-093917-853',
+       'https://www.inherentresolve.mil/Portals/14/Documents/Strike%20Releases/2017/02February/20170203%20Strike%20Release%20Final.pdf?ver=2017-02-03-074516-280',
+       'https://www.inherentresolve.mil/Portals/14/Documents/Strike%20Releases/2017/02February/20170217%20Strike%20Release%20Final.pdf?ver=2017-02-17-093725-800',
+       'https://www.inherentresolve.mil/Portals/14/Documents/Strike%20Releases/2017/10October/20171023%20Strike%20Release.pdf?ver=2017-10-23-033103-287',
+       'https://www.inherentresolve.mil/Portals/14/Documents/Strike%20Releases/2017/11November/20171103%20CJTF-OIR%20Strike%20Release.pdf?ver=2017-11-03-065958-837']
+
 
 # handle button events
 def press(button):
@@ -17,7 +31,11 @@ def press(button):
             out = app.getEntry("Output")
         else:
             out = ""
-        print("Existing File: " + exist + " | Output Path: " + out)
+
+        data = ex.data_from_urls(urls, out)
+        # TODO REMOVE THIS TOO
+        print(data)
+
 
 
 
@@ -31,9 +49,9 @@ app.setLabelBg("title", "green")
 app.setLabelFg("title", "gold")
 
 app.addLabelFileEntry("Existing Data")
-app.setEntryDefault("Existing Data", "Optional")
+app.setEntryDefault("Existing Data", exist)
 app.addLabelDirectoryEntry("Output")
-app.setEntryDefault("Output", "Select path...")
+app.setEntryDefault("Output", out)
 
 # link the buttons to the function called press
 app.addButtons(["Go", "Cancel"], press)
